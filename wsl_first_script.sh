a50175c229f0ba6094d74976afd49252dbffa682
iure de oliveira radin <iureradin@hotmail.com>
2022-11-08 23:38:31 -0300

    Criada função que adiciona alias ao bashrc

--- wsl_first_script.sh
+++ wsl_first_script.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-sudo apt update -y
+#sudo apt update -y
 
-apt install nano vim htop nmap tcpdump net-tools git zsh -y
+#apt install nano vim htop nmap tcpdump net-tools git zsh -y
+
+function _add_alias() {
+    ALIAS=($(awk '{print $2}' alias.txt | awk -F '=' '{print $1}'))
+    if [[ -n $ALIAS ]]; then
+        for((cont=0;cont<${#ALIAS[@]};cont++)); do
+            echo "alias encontrado: ${ALIAS[cont]}"
+            sed -i '/alias '${ALIAS[cont]}'/d' ~/.bashrc
+        done
+        cat alias.txt >> ~/.bashrc
+    else
+        echo "Nenhum alias foi encontrado"
+    fi
+}
+
+_add_alias
+
